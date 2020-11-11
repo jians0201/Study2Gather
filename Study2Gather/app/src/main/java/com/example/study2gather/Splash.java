09b2b438f0f286dfb5ab708eca017d60fad2da73
Bk49 <ericngyongwei@gmail.com>
2020-11-11 13:16:42 +0800

    adding space

--- Study2Gather/app/src/main/java/com/example/study2gather/Splash.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Splash.java
@@ -2,4 +2,6 @@ package com.example.study2gather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Splash extends AppCompatActivity{}
+public class Splash extends AppCompatActivity{
+
+}
