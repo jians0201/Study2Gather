c780982858a4dfce3cb63636755265146c052b8c
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2020-11-14 15:52:43 +0800

    Added Login and Register
    Login page
    - added hints
    - changed hint colour and text colour to white
    - created validation function and moved validations
    
    Register page
    - fixed DOB textfield
    - created validation function and moved validations
    - used setError() instead of Toast to show error
    
    Firebase
    - Added dependencies and updated gradle
    - Added Firebase Auth
    - Added Register User function
    - Added Login User function

--- Study2Gather/app/src/main/java/com/example/study2gather/Login.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Login.java
@@ -2,41 +2,93 @@ package com.example.study2gather;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
+    private EditText loginEmail, loginPword;
+    private ProgressBar pgbar;
+
+    private Intent i;
+
+    private FirebaseAuth fAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Launch the layout -> splash.xml
         setContentView(R.layout.login);
+        loginEmail = findViewById(R.id.loginEmailAddress);
+        loginPword = findViewById(R.id.loginPassword);
+        pgbar = findViewById(R.id.loginProgressBar);
+        fAuth = FirebaseAuth.getInstance();
+    }
+
+    private boolean validateUserData(String email, String password) {
+        if (email.isEmpty()) {
+            loginEmail.setError("Email is Required!");
+            loginEmail.requestFocus();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            loginEmail.setError("Enter a Valid Email!");
+            loginEmail.requestFocus();
+        } else if (password.isEmpty()) {
+            loginPword.setError("Password is Required!");
+            loginPword.requestFocus();
+        } else if (password.length() < 8) {
+            loginPword.setError("Enter a Valid Password!");
+            loginPword.requestFocus();
+        }
+        else {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void onClick(View v){
-        Intent i;
         switch(v.getId()){
             case R.id.loginLoginButton:
-                TextView emailTxt = (TextView)findViewById(R.id.loginEmailAddress);
-                TextView passwordTxt = (TextView)findViewById(R.id.loginPassword);
-                String emailReg = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPword.getText().toString().trim();
 
-                if(!emailTxt.getText().toString().matches(emailReg) || emailTxt.getText().toString().length() == 0 || passwordTxt.getText().toString().length() < 8){
-                    Toast.makeText(getApplicationContext(), "Your Login Details are incorrect!", Toast.LENGTH_SHORT).show();
-                }else{
-                    i = new Intent(getApplicationContext(), Home.class);
-                    startActivity(i);
+                if (validateUserData(email, password)) {
+                    pgbar.setVisibility(View.VISIBLE);
+                    fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+//                                Toast.makeText(Login.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                pgbar.setVisibility(View.GONE);
+//                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //leave it first I might need later on
+                                i = new Intent(getApplicationContext(), Home.class);
+                                startActivity(i);
+                            } else {
+                                Toast.makeText(Login.this, "Failed to login! Email or Password is incorrect!", Toast.LENGTH_LONG).show();
+                                pgbar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+
+
                 }
+
                 break;
             case R.id.loginRegisterButton:
                 i = new Intent(getApplicationContext(), Registration.class);
                 startActivity(i);
+                break;
         }
     }
 }
--- Study2Gather/app/src/main/java/com/example/study2gather/Registration.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Registration.java
@@ -4,14 +4,23 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Patterns;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -19,43 +28,97 @@ import java.util.Date;
 
 public class Registration extends AppCompatActivity implements View.OnClickListener{
     // Global vars
-    private boolean sexValidate = false;
     private String DOBStr = "";
+    private String gender = "";
+
+    private TextView DOBTextField;
+    private EditText rgUname, rgEmail, rgPword, rgCfmPword;
+    private ProgressBar pgbar;
+    private RadioButton rgSexMale, rgSexFemale;
+
+    private FirebaseAuth fAuth;
+    private FirebaseDatabase fDb;
+
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Launch the layout -> splash.xml
         setContentView(R.layout.registration);
+        DOBTextField = findViewById(R.id.registerDOBTxt);
+        rgUname = findViewById(R.id.registerUsername);
+        rgEmail = findViewById(R.id.registerEmail);
+        rgPword = findViewById(R.id.registerPassword);
+        rgCfmPword = findViewById(R.id.registerConfirmPassword);
+        fAuth = FirebaseAuth.getInstance();
+        rgSexMale = findViewById(R.id.registerSexMale);
+        rgSexFemale = findViewById(R.id.registerSexFemale);
+        pgbar = findViewById(R.id.registerProgressBar);
     }
 
     // Handles radio button validations
     public void onRadioButtonClicked(View v){
         boolean checked = ((RadioButton) v).isChecked();
-
-
+        rgSexFemale.setError(null);
+        rgSexMale.setError(null);
         switch(v.getId()){
             case R.id.registerSexMale:
                 if(checked){
                     // Code for checking male
-                    sexValidate = true;
+                    gender = "male";
                 }
                 break;
             case R.id.registerSexFemale:
                 if(checked){
-                    // Code for checking female // Ignore dup branch, 2 branches needed for data saving (FIREBASE)
-                    sexValidate = true;
+                    // Code for checking female
+                    gender = "female";
                 }
                 break;
         }
     }
 
+    private boolean validateUserData(String uname, String email, String pword, String cfmPword) {
+        // For email validations
+//        String emailReg = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+        // Validations
+        if(uname.length() < 4 || uname.length() > 18 ){
+            // Username
+            rgUname.setError("Username must be 4-18 characters!");
+            rgUname.requestFocus();
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches() || email.length() < 0 ){ //edtTxtStrAry.get(1).matches(emailReg) replaced with patterns library
+            // Email
+            rgEmail.setError("Enter a valid Email!");
+            rgEmail.requestFocus();
+        }else if(pword.length() < 8){
+            // Password
+            rgPword.setError("Password must be at least 8 characters");
+            rgPword.requestFocus();
+        }else if(!cfmPword.equals(pword)){
+            // Confirm Password
+            rgCfmPword.setError("Passwords do not match!");
+            rgCfmPword.requestFocus();
+        }else if(gender.length()==0){
+            // Sex RadioButton
+            rgSexFemale.setError("Select a Gender!");
+            rgSexMale.setError("Select a Gender!");
+        }else if(DOBStr.length() == 0){
+            DOBTextField.setError("Enter a Date of Birth!");
+            DOBTextField.requestFocus();
+        }else{
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public void onClick(View v){
-        Intent i;
+//        Intent i;
         switch(v.getId()){
             // Handles for DatePicker
             case R.id.registerDOBBtn:
+                DOBTextField.setError(null);
                 Calendar calendar = Calendar.getInstance();
                 DatePickerDialog dpd;
 
@@ -66,55 +129,56 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                 dpd = new DatePickerDialog(Registration.this, new DatePickerDialog.OnDateSetListener(){
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth){
-                        TextView DOBLabel = findViewById(R.id.registerDOBLabel);
                         DOBStr = dayOfMonth+" / "+(month+1)+" / "+year;
-                        DOBLabel.setText(DOBStr);
-                        // Saving of data to firebase can be done here to get the DOB input!
+                        DOBTextField.setText(DOBStr);
                     }
                 }, year, month, day);
                 dpd.getDatePicker().setMaxDate(new Date().getTime());
                 dpd.show();
                 break;
+
             // Handles for Register Button
             case R.id.registerButton:
-                // Input validation
-                ArrayList<EditText> edtTxtAry = new ArrayList<>();
-                edtTxtAry.add(findViewById(R.id.registerUsername));
-                edtTxtAry.add(findViewById(R.id.registerEmail));
-                edtTxtAry.add(findViewById(R.id.registerPassword));
-                edtTxtAry.add(findViewById(R.id.registerConfirmPassword));
+                //Get text fields
+                String username = rgUname.getText().toString().trim();
+                String email = rgEmail.getText().toString().trim();
+                String password = rgPword.getText().toString().trim();
+                String confirmPassword = rgCfmPword.getText().toString().trim();
 
-                // Change to String, push into new array
-                ArrayList<String> edtTxtStrAry = new ArrayList<>();
-                for(EditText edtTxt : edtTxtAry) edtTxtStrAry.add(edtTxt.getText().toString());
-
-                // For email validations
-                String emailReg = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-
-                // Validations
-                if(edtTxtStrAry.get(0).length() < 4 || edtTxtStrAry.get(0).length() > 18 ){
-                    // Username
-                    Toast.makeText(Registration.this, "Username must be in between 4 to 18 characters!", Toast.LENGTH_SHORT).show();
-                }else if(!edtTxtStrAry.get(1).matches(emailReg) || edtTxtStrAry.get(1).length() < 0 ){
-                    // Email
-                    Toast.makeText(Registration.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
-                }else if(edtTxtStrAry.get(2).length() < 8){
-                    // Password
-                    Toast.makeText( Registration.this, "Password must be 8 characters or above!", Toast.LENGTH_SHORT).show();
-                }else if(!edtTxtStrAry.get(3).equals(edtTxtStrAry.get(2))){
-                    // Confirm Password
-                    Toast.makeText(Registration.this, "Password confirmed does not tally!", Toast.LENGTH_SHORT).show();
-                }else if(!sexValidate){
-                    // Sex RadioButton
-                    Toast.makeText(Registration.this, "Please choose your sex!", Toast.LENGTH_SHORT).show();
-                }else if(DOBStr.length() == 0){
-                    Toast.makeText(Registration.this, "Please input your Date of Birth!", Toast.LENGTH_SHORT).show();
-                }else{
-                    // Use edtTxtStrAry to get input data for saving to FireBase
-                    Toast.makeText(Registration.this, "Created new user successfully", Toast.LENGTH_SHORT).show();
-                    i = new Intent(getApplicationContext(), Login.class);
-                    startActivity(i);
+                //Validate Inputs
+                if (validateUserData(username, email, password, confirmPassword)) {
+                    //Show progress bar
+                    pgbar.setVisibility(View.VISIBLE);
+                    //Create user
+                    fAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        UserObj user = new UserObj(username,email,password,gender,DOBStr);
+                                        fDb.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(Registration.this, "Created new user successfully", Toast.LENGTH_SHORT).show();
+                                                            pgbar.setVisibility(View.GONE); //Remove progress bar
+                                                            i = new Intent(getApplicationContext(), Login.class);
+                                                            startActivity(i);
+                                                        } else {
+                                                            Toast.makeText(Registration.this, "Failed to Register! Try Again!", Toast.LENGTH_SHORT).show();
+                                                            pgbar.setVisibility(View.GONE); //Remove progress bar
+                                                        }
+                                                    }
+                                                });
+                                    } else {
+                                        Toast.makeText(Registration.this, "Failed to Register! Try Again!", Toast.LENGTH_SHORT).show();
+                                        pgbar.setVisibility(View.GONE); //Remove progress bar
+                                    }
+                                }
+                            });
                 }
+                break;
         }
     }
 }
