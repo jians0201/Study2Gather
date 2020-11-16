8d3a63ea4179f08b944f892f21a433e02dbdd827
Bk49 <ericngyongwei@gmail.com>
2020-11-16 10:29:38 +0800

    Button linkings

--- Study2Gather/app/src/main/java/com/example/study2gather/Home.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Home.java
@@ -1,7 +1,27 @@
 package com.example.study2gather;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Launch the layout -> splash.xml
+        setContentView(R.layout.home);
+    }
 
+    public void onClick(View v){
+        Intent i;
+
+        switch(v.getId()){
+            case R.id.homeProfileButton:
+               i = new Intent(getApplicationContext(), Profile.class);
+               startActivity(i);
+               break;
+        }
+    }
 }
--- Study2Gather/app/src/main/java/com/example/study2gather/Messages.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Messages.java
@@ -1,7 +1,27 @@
 package com.example.study2gather;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Messages extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Launch the layout -> splash.xml
+        setContentView(R.layout.messages);
+    }
 
+    public void onClick(View v){
+        Intent i;
+
+        switch(v.getId()){
+            case R.id.messagesChatButton:
+                i = new Intent(getApplicationContext(), Chat.class);
+                startActivity(i);
+                break;
+        }
+    }
 }
