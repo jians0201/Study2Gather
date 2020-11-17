4e7ab6e37f3eaf78454f07cc785f260e3fc926d5
Jian Sheng <64114913+jians0201@users.noreply.github.com>
2020-11-17 22:50:32 +0800

    Added Tab Bar & Floating Action Btn

--- Study2Gather/app/src/main/java/com/example/study2gather/Home.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/Home.java
@@ -2,16 +2,40 @@ package com.example.study2gather;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity{
+
+    private BottomNavigationView navView;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Launch the layout -> splash.xml
         setContentView(R.layout.home);
+        navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_forum, R.id.navigation_courses)
+                .build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     public void onClick(View v){
